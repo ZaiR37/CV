ce93b009f6b82e1f27df042a4577fe590aee5d48
ZaiR37 <70493871+ZaiR37@users.noreply.github.com>
2023-06-25 23:16:58 +0700

    Combining all section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
+import AboutSection from './components/AboutSection.vue';
+import ContactMeSection from './components/ContactMeSection.vue';
+import ProjectSection from './components/ProjectSection.vue';
+import WebCover from './components/WebCover.vue';
 import WebHeader from './components/WebHeader.vue'
 
 </script>
 
 <template>
   <WebHeader></WebHeader>
+  <WebCover></WebCover>
+  <AboutSection></AboutSection>
+  <ProjectSection></ProjectSection>
+  <ContactMeSection></ContactMeSection>
 </template>
 
 <style scoped>
